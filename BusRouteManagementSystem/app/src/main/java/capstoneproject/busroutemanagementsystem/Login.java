12574c00510599aa339400faf03b59387b39567a
wizcalil <colecaliph@gmail.com>
2015-01-20 06:11:13 -0500

    update

---
+++ BusRouteManagementSystem/app/src/main/java/capstoneproject/busroutemanagementsystem/Login.java
@@ -0,0 +1,18 @@
+package capstoneproject.busroutemanagementsystem;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by CaliphCole on 01/20/2015.
+ */
+public class Login extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+}
